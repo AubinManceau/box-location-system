633e8d2af154bdefa8fa759ff621ba3cd0595972
AubinManceau <aubinmanceau0@gmail.com>
2025-02-18 11:53:14 +0100

    update Bill getter

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Bill;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
     public function index()
     {
-        $bills = Bill::where('payment_date', null)->get();
+        $bills = Bill::where('payment_date', null)
+        ->whereHas('contract.box', function ($query) {
+            $query->where('user_id', Auth::user()->id);
+        })
+        ->get();
         return view('payment.index', [
             'bills' => $bills
         ]);
--- app/Http/Controllers/TaxeController.php
+++ app/Http/Controllers/TaxeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Bill;
 
 class TaxeController extends Controller
@@ -10,11 +11,17 @@ class TaxeController extends Controller
     public function index()
     {
         $year = date('Y');
-        $bills = Bill::where('created_at', 'like', $year . '%')->get();
+        $bills = Bill::where('created_at', 'like', $year . '%')
+            ->whereHas('contract.box', function ($query) {
+                $query->where('user_id', Auth::user()->id);
+            })
+            ->get();
         $annualTurnover = 0;
 
         foreach ($bills as $bill) {
-            $annualTurnover += $bill->payment_amount;
+            if($bill->payment_date){
+                $annualTurnover += $bill->payment_amount;
+            }
         }
 
         return view('taxe.index', [
